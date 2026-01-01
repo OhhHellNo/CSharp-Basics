514f81c612596917dff521beadf4be603c040a40
viperofficailll <sharadhapokharel777@gmal.com>
2026-01-01 11:50:59 +0545

    add class generics conditionals and overloading

---
+++ classes.cs
@@ -0,0 +1,12 @@
+/* class is a bundle of related code . it can be used to create objects oop*/
+
+
+class program
+{
+
+    public static void main(string[] args)
+    {
+
+    }
+
+}
---
+++ conditionals.cs
@@ -0,0 +1,10 @@
+class program
+{
+
+    public static void Main(string[] args)
+    {
+        int x = 5;
+        int y = 6;
+        (x > y) ? System.Console.WriteLine("X is grater") : Console.WriteLine("y is greater");
+    }
+}
